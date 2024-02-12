481b9951a2a5161bd6b7493a02d542c8dd40ba7c
Felipe Maion <felipe.maion@gmail.com>
2024-02-12 03:53:16 -0300

    Implement Hash password for Manager

--- flask_app/app/models/manager.py
+++ flask_app/app/models/manager.py
@@ -1,10 +1,46 @@
+# from app.extensions import db
+
+# class Manager(db.Model):
+#     __tablename__ = 'manager'
+#     manager_id = db.Column(db.Integer, primary_key=True)
+#     user_name = db.Column(db.String(100))
+#     password = db.Column(db.String(255))
+
+#     def __repr__(self):
+#         return f'<Manager "{self.title}">'
+
+import hashlib
 from app.extensions import db
 
+
 class Manager(db.Model):
-    __tablename__ = 'manager'
+    __tablename__ = "manager"
     manager_id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(100))
-    password = db.Column(db.String(255))
-    
+    password = db.Column(db.String(255))  # Storing hashed password
+
     def __repr__(self):
-        return f'<Manager "{self.title}">'
+        return f'<Manager "{self.user_name}">'
+
+    @property
+    def _password(self):
+        return self._password
+
+    @_password.setter
+    def _password(self, _password):
+        # Hash the password before storing it
+        self.password = hashlib.sha256(_password.encode()).hexdigest()
+
+
+# Register event listener to hash password before insertion
+@db.event.listens_for(Manager, "before_insert")
+def hash_password_before_insert(mapper, connection, target):
+    if target.password:
+        target.password = hashlib.sha256(target.password.encode()).hexdigest()
+
+
+# Register event listener to hash password before update
+@db.event.listens_for(Manager, "before_update")
+def hash_password_before_update(mapper, connection, target):
+    if target.password:
+        target.password = hashlib.sha256(target.password.encode()).hexdigest()
