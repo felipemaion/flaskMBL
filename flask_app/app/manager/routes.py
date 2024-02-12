a8de0c3e6e61abd8068f4c503a6934b47c52358a
Felipe Maion <felipe.maion@gmail.com>
2024-02-12 03:53:16 -0300

    Implement Hash password for Manager

--- flask_app/app/manager/routes.py
+++ flask_app/app/manager/routes.py
@@ -3,36 +3,48 @@ from app.extensions import db
 from flask import jsonify, request
 from app.models.manager import Manager
 
-@bp.route('/', methods=['POST'])
+
+@bp.route("/", methods=["POST"])
 def create_manager():
     data = request.json
-    new_manager = Manager(user_name=data['user_name'], password=data['password'])
+    new_manager = Manager(user_name=data["user_name"], password=data["password"])
     db.session.add(new_manager)
     db.session.commit()
     return jsonify({"message": "Manager created successfully."}), 201
 
-@bp.route('/', methods=['GET'])
+
+@bp.route("/", methods=["GET"])
 def get_managers():
     managers = Manager.query.all()
-    managers_data = [{"manager_id": manager.manager_id, "user_name": manager.user_name} for manager in managers]
+    managers_data = [
+        {
+            "manager_id": manager.manager_id,
+            "user_name": manager.user_name,
+            "password": manager.password,
+        }
+        for manager in managers
+    ]
     return jsonify(managers_data)
 
-@bp.route('/<int:manager_id>', methods=['GET'])
+
+@bp.route("/<int:manager_id>", methods=["GET"])
 def get_manager(manager_id):
     manager = Manager.query.get_or_404(manager_id)
     manager_data = {"manager_id": manager.manager_id, "user_name": manager.user_name}
     return jsonify(manager_data)
 
-@bp.route('/<int:manager_id>', methods=['PUT'])
+
+@bp.route("/<int:manager_id>", methods=["PUT"])
 def update_manager(manager_id):
     manager = Manager.query.get_or_404(manager_id)
     data = request.json
-    manager.user_name = data['user_name']
-    manager.password = data['password']
+    manager.user_name = data["user_name"]
+    manager.password = data["password"]
     db.session.commit()
     return jsonify({"message": "Manager updated successfully."})
 
-@bp.route('/<int:manager_id>', methods=['DELETE'])
+
+@bp.route("/<int:manager_id>", methods=["DELETE"])
 def delete_manager(manager_id):
     manager = Manager.query.get_or_404(manager_id)
     db.session.delete(manager)
--- flask_app/config.py
+++ flask_app/config.py
@@ -5,8 +5,10 @@ config = dotenv_values(".env")
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    # SECRET_KEY = config['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = config['DATABASE_URI']\
-        or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  
+    SECRET_KEY = config["SECRET_KEY"]
+    SQLALCHEMY_DATABASE_URI = config["DATABASE_URI"] or "sqlite:///" + os.path.join(
+        basedir, "app.db"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
